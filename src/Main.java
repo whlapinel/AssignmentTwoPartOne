9692249e0bc8873b3c75ea34e7ebbf0de2389c60
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-14 23:12:05 -0400

    Made a bunch of changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Enter title: ");
+        ArrayList<Movie> library = new ArrayList<>();
+        System.out.println("Provide number of movies to enter in library: ");
         Scanner sc = new Scanner(System.in);
+        int runtimes = Integer.parseInt(sc.nextLine());
+        for (int x=0; x<runtimes; ++x) {
+            Movie newMovie = enterMovie(sc);
+            library.add(newMovie);
+            for (Movie movie : library) {
+                movie.print();
+            }
+        }
+
+    }
+    public static Movie enterMovie(Scanner sc) {
+        System.out.println("Please enter movie information as follows.");
+        System.out.println("Enter title: ");
         String title = sc.nextLine();
-        Movie newMovie = new Movie(title);
-        newMovie.print();
+        System.out.println("Enter director: ");
+        String director = sc.nextLine();
+        System.out.println("Enter actors: ");
+        String actors = sc.nextLine();
+        System.out.println("Enter genre: ");
+        String genre = sc.nextLine();
+        System.out.println("Enter duration: ");
+        int duration = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter year: ");
+        int year = Integer.parseInt(sc.nextLine());
+        return new Movie(title, year, director, duration, actors, genre);
+    }
+    public static void getMaxRating() {
+        System.out.println("Top rated movie is: "); //FIXME
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,12 +1,33 @@
 public class Movie {
     private String title;
+    private int year;
+    private String director;
+    private int duration;
+    private String actors;
+    private String genre;
+    private int rating;
 
-    public Movie(String title) {
+    public Movie(String title, int year, String director, int duration, String actors, String genre) {
         this.title = title;
+        this.year = year;
+        this.director = director;
+        this.duration = duration;
+        this.actors = actors;
+        this.genre = genre;
+        this.rating = (int) (Math.random() * 10);
     }
 
     public void print() {
-        System.out.println(this.title);
-
+        System.out.println("**********************************");
+        System.out.println("information about: " + this.title);
+        System.out.println("**********************************");
+        System.out.println("title: " + this.title);
+        System.out.println("year: " + this.year);
+        System.out.println("director: " + this.director);
+        System.out.println("duration: " + this.duration);
+        System.out.println("actors: " + this.actors);
+        System.out.println("genre: " + this.genre);
+        System.out.println("rating: " + this.rating);
+        System.out.println("**********************************");
     }
 }
