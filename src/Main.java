9f308b80e4f48516f721ea75b4ff6a0ec62d8416
whlapinel <140720302+whlapinel@users.noreply.github.com>
2023-09-18 16:35:25 -0400

    Fixed rating to make it 1-10 instead of 0-9, and added index n = i + 1 to make it 1-10.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+Go to repl.it and implement  a class called Movie  with the following attributes:
+
+        Title, Year, Director, Duration, Actors, Genre, Rating
+
+        Create the appropriate methods:
+        You should include a method that prints all the movie details.
+
+        -----------------------------------------
+
+        In main create an array of  4 Movies.  Create instances of 4 movies and  enter all relevant information from the
+         keyboard. The rating for each movie should be randomly generated on a scale of 1 to 10.  Keep track of the
+         movie with the highest rating and the movie with the lowest rating.  Iterate the array and print the title and
+         rating of all movies in the array of movies. Then print the title of the movie with the highest and lowest rating.
+
+        When you are finished share your repl url with me.
+*/
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,14 +26,7 @@ public class Main {
         System.out.println("Provide number of movies to enter in library: ");
         Scanner sc = new Scanner(System.in);
         autoEnterMovie(library,sc);
-/*        int runtimes = Integer.parseInt(sc.nextLine());
-        for (int x=0; x<runtimes; ++x) {
-            Movie newMovie = UserEnterMovie(sc);
-            library.add(newMovie);
-            for (Movie movie : library) {
-                movie.print();
-            }
-        }*/
+
         System.out.println("Movie library: ");
         for (Movie movie : library) {
             movie.print();
@@ -26,31 +37,18 @@ public class Main {
         System.out.println(getBottomRated(library));
     }
     public static void autoEnterMovie(ArrayList<Movie> library, Scanner sc) {
+//        String[] actorArray = String[]{"Bruce Willis", "Bruce Lee", "Jane Fonda", "Jackie Chan", ""};
+
         System.out.println("Enter number of movies to generate: ");
         int numMovies = Integer.parseInt(sc.nextLine());
         for (int i=0; i<numMovies; ++i) {
+            int n = i + 1;
             Movie newMovie = new Movie(
-                    "title" + i, 1980, "director" + i,
-                    90, "actors" + i, "genre" + i);
+                    "title " + n, 1980, "director " + n,
+                    90, "actors " + n, "genre " + n);
             library.add(newMovie);
         }
     }
-/*    public static Movie UserEnterMovie(Scanner sc) {
-        System.out.println("Please enter movie information as follows.");
-        System.out.println("Enter title: ");
-        String title = sc.nextLine();
-        System.out.println("Enter director: ");
-        String director = sc.nextLine();
-        System.out.println("Enter actors: ");
-        String actors = sc.nextLine();
-        System.out.println("Enter genre: ");
-        String genre = sc.nextLine();
-        System.out.println("Enter duration: ");
-        int duration = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter year: ");
-        int year = Integer.parseInt(sc.nextLine());
-        return new Movie(title, year, director, duration, actors, genre);
-    }*/
 
     public static String getTopRated(ArrayList<Movie> library) {
         Movie topRatedMovie = library.get(0);
